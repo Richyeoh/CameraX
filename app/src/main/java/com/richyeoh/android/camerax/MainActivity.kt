466d53f7d2fa5287de9c6a3b8160ce13eb4e15eb
泽被苍生 <56948566+Richyeoh@users.noreply.github.com>
2020-05-18 23:34:11 +0800

    Completed a camera preview feature

--- app/src/main/java/com/richyeoh/android/camerax/MainActivity.kt
+++ app/src/main/java/com/richyeoh/android/camerax/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.richyeoh.android.camerax
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val PERMISSION_REQ_CODE = 0x001
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (allPermissionsGranted()) {
+            bindCamera()
+        } else {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_REQ_CODE)
+        }
+    }
+
+    private fun bindCamera() {
+        view_finder.toggleCamera()
+        view_finder.bindToLifecycle(this)
+    }
+
+    private fun allPermissionsGranted() = PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQ_CODE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            bindCamera()
+        } else {
+            finish()
+        }
     }
 }
